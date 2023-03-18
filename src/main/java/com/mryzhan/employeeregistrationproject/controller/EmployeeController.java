46dc60e823ac63fef29d688b46addb61bcf367f9
meekkeek <ryzhan43@gmail.com>
2023-03-18 16:01:02 -0400

    EmployeeController updated

--- src/main/java/com/mryzhan/employeeregistrationproject/controller/EmployeeController.java
+++ src/main/java/com/mryzhan/employeeregistrationproject/controller/EmployeeController.java
@@ -1,15 +1,25 @@
 package com.mryzhan.employeeregistrationproject.controller;
 
+import com.mryzhan.employeeregistrationproject.bootstrap.DataGenerator;
+import com.mryzhan.model.Employee;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
 
     @GetMapping("/register")
-    public String createEmployee() {
+    public String createEmployee(Model model) {
+
+        model.addAttribute("states", DataGenerator.getAllStates());
+        model.addAttribute("employee", new Employee());
 
         return "employee/employee-create";
     }
