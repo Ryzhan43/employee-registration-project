c53f577e28efa0f3390d778b3380e7d4f69723d3
meekkeek <ryzhan43@gmail.com>
2023-03-18 15:01:19 -0400

    Initial commit

---
+++ src/main/java/com/mryzhan/model/Employee.java
@@ -0,0 +1,22 @@
+package com.mryzhan.model;
+
+import lombok.*;
+
+import java.time.LocalDate;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String address;
+    private String address2;
+    private String city;
+    private String state;
+    private String zipCode;
+
+}
